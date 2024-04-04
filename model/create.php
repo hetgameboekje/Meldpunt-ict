a9192654b3ef04cbe8c7606362d53400ee6ab41f
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2024-04-04 13:21:22 +0200

    Create create.php

---
+++ model/create.php
@@ -0,0 +1,25 @@
+<?php 
+// =============================================
+// Pagina krijgt door post info over
+// class maken voor Create van CRUD 
+// Moet het een bestelling, Registratie gebruiker , Registratie product ect -> functies
+// class maken voor Create van CRUD 
+// 
+//
+// =============================================
+require "db.php";
+
+class createData {
+
+    public function addOrder($orderData) 
+    {
+        // het toevoegen van de bestelling
+    }
+
+    public function addUser($userRegister) 
+    {
+        // het toevoegen van de bestelling
+    }
+
+    public function 
+}
